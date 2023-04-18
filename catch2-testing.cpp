252db4afad8f7d05801a7ca70e6a5ad74ba76279
Emad <ic22b050@technikum-wien.at>
2023-04-18 18:23:00 +0200

    emad add catch2

--- catch2-testing.cpp
+++ catch2-testing.cpp
@@ -2,7 +2,7 @@
 // Created by Tae Takemi on 18.04.2023.
 //
 #define CATCH_CONFIG_MAIN
-#include "catch2/catch.hpp"
+#include <catch2/catch_all.hpp>
 
 TEST_CASE("test assignSeat function") {
 struct Classroom *classroom = generateClassroom(3, 3);
