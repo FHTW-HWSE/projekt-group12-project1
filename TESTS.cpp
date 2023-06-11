30d95dd4436b745c3481cddc1d3e7e03e08d773e
momoKitsune <tea.takemi@mailbox.org>
2023-06-12 01:18:57 +0200

    2 Catch2 Test?

--- TESTS.cpp
+++ TESTS.cpp
@@ -42,3 +42,69 @@ TEST_CASE("markStudentAsInfected marks the specified student as infected") {
     free(classroom);
 
 }
+
+TEST_CASE("getDirectNeighbors returns the direct neighbors of the specified seat") {
+    //create a sample classroom
+    int rows = 2;
+    int cols = 2;
+    struct seat** classroom = generateClassroom(rows, cols, "Test Room");
+
+    //add a student to the classroom
+    strcpy(classroom[0][0].ID, "123456789");
+
+    //call the function to get the direct neighbors of the specified seat
+    struct seat** neighbors = getDirectNeighbors(classroom, rows, cols, 0, 0);
+
+    //check if the direct neighbors are returned
+    REQUIRE(strcmp(neighbors[0][0].ID, "123456789") == 0);
+    REQUIRE(strcmp(neighbors[0][1].ID, "0") == 0);
+    REQUIRE(strcmp(neighbors[1][0].ID, "0") == 0);
+    REQUIRE(strcmp(neighbors[1][1].ID, "0") == 0);
+
+    //free memory
+    free(classroom);
+    free(neighbors);
+
+}
+
+TEST_CASE("getIndirectNeighbors returns the indirect neighbors of the specified seat") {
+    //create a sample classroom
+    int rows = 2;
+    int cols = 2;
+    struct seat** classroom = generateClassroom(rows, cols, "Test Room");
+
+    //add a student to the classroom
+    strcpy(classroom[0][0].ID, "123456789");
+
+    //call the function to get the indirect neighbors of the specified seat
+    struct seat** neighbors = getIndirectNeighbors(classroom, rows, cols, 0, 0);
+
+    //check if the indirect neighbors are returned
+    REQUIRE(strcmp(neighbors[0][0].ID, "0") == 0);
+    REQUIRE(strcmp(neighbors[0][1].ID, "0") == 0);
+    REQUIRE(strcmp(neighbors[1][0].ID, "0") == 0);
+    REQUIRE(strcmp(neighbors[1][1].ID, "0") == 0);
+
+    //free memory
+    free(classroom);
+    free(neighbors);
+
+}
+
+TEST_CASE("generateClassroom generates a classroom with the specified rows and columns") {
+    //create a sample classroom
+    int rows = 2;
+    int cols = 2;
+    struct seat** classroom = generateClassroom(rows, cols, "Test Room");
+
+    //check if the classroom is generated with the specified rows and columns
+    REQUIRE(strcmp(classroom[0][0].ID, "0") == 0);
+    REQUIRE(strcmp(classroom[0][1].ID, "0") == 0);
+    REQUIRE(strcmp(classroom[1][0].ID, "0") == 0);
+    REQUIRE(strcmp(classroom[1][1].ID, "0") == 0);
+
+    //free memory
+    free(classroom);
+
+}
+
